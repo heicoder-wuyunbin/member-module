3fb4ca645dadc26a0c83fc5dbf3b230f2a0bcfac
wuyunbin <617772977@qq.com>
2026-03-25 19:55:01 +0800

    init

---
+++ src/main/java/com/quadra/member/MemberModuleApplication.java
@@ -0,0 +1,13 @@
+package com.quadra.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quadra/member/MemberModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quadra.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
